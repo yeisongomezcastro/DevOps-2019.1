3af8a2b61db1b9e33415c82248443dfa9324c99f
yeisongomezcastro <38989187+yeisongomezcastro@users.noreply.github.com>
2019-08-15 10:40:12 -0500

    Update MembershipManagementTest.java

--- src/test/java/io/quind/training/basicconsoleapp/adapters/userinterface/MembershipManagementTest.java
+++ src/test/java/io/quind/training/basicconsoleapp/adapters/userinterface/MembershipManagementTest.java
@@ -1,13 +1,10 @@
 package io.quind.training.basicconsoleapp.adapters.userinterface;
 
-import io.quind.training.basicconsoleapp.domain.Member;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,24 +29,5 @@ class MembershipManagementTest {
         when(mockedReader.nextInt()).thenReturn(in);
         int result = mm.getChoice();
         assertEquals(expected, result);
-
-
     }
-    
-    @DisplayName("Should insert correct integer value")
-    @ParameterizedTest(name = "{index} => Input Value = {0}")
-    @ValueSource(ints = {1, 2, 3, -1})
-    void getChoice(int in) {
-        int expected = in;
-        when(mockedReader.nextInt()).thenReturn(in);
-        String result = mm.addMembers(buildList());
-        assertEquals(expected,result);
-
-    }
-
-    private LinkedList<Member> buildList() {
-        LinkedList<Member> list = new LinkedList<>();
-        return list;
-    }
-
 }
