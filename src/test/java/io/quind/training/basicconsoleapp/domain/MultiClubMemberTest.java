9984cb18eacc53629c49f562af8a5f4653bb9352
yeisongomezcastro <38989187+yeisongomezcastro@users.noreply.github.com>
2019-08-15 10:52:10 -0500

    Update MultiClubMemberTest.java

--- src/test/java/io/quind/training/basicconsoleapp/domain/MultiClubMemberTest.java
+++ src/test/java/io/quind/training/basicconsoleapp/domain/MultiClubMemberTest.java
@@ -18,7 +18,7 @@ class MultiClubMemberTest {
     void testToString(int memberID, char memberType, String name, double fees, int membershipPoints) {
     	
 	    SingleClubMember member = new SingleClubMember(memberID, memberType, name, fees, club);
-	    String expected = String.format("%s, %s, %s, %.1f, %s", memberID, memberType, name, fees, club);
+	    String expected = String.format("%s, %s, %s, %.1f, %s", memberID, memberType, name, fees) + "," + club;
 	    assertEquals(expected, member.toString());
         
     }
